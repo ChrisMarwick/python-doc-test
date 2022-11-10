3f32e1ac9935e76783b9842ccab3ed8cacfa303e
Chris Marwick <chris.marwick@prospa.com>
2022-11-10 15:10:57 +1100

    test3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,9 +14,16 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx_multiversion",
+]
 
 templates_path = ['_templates']
+html_sidebars = {
+    '**': [
+        'versioning.html',
+    ]
+}
 exclude_patterns = []
 
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 def main():
     """This is the trunk version of my changes"""
-    print('Howdy')
+    print('Howdy1')
 
 if __name__ == '__main__':
     main()
